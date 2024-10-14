2ef188e6cf5363abba7ef4ec1c06321d11b94e8b
Treacle <toandq@cyberk.io>
2024-10-14 22:00:38 +0700

    add simple toast

--- src/components/telegram-main.tsx
+++ src/components/telegram-main.tsx
@@ -2,9 +2,19 @@
 
 import { useTelegramUser } from "@/telegram/use-telegram-user";
 import { MaxWidthWrapper } from "./ui/max-width-wrapper";
+import { useToast } from "@/hooks/use-toast";
 
 export const TelegramMain = () => {
 	const { user } = useTelegramUser();
+	const { toast } = useToast();
+
+	if (!user) {
+		toast({
+			title: "Error",
+			description: "User not found",
+			variant: "destructive",
+		});
+	}
 
 	return (
 		<MaxWidthWrapper>
