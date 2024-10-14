aef281c319b8df9a49820a07548c9448cb520d27
Treacle <toandq@cyberk.io>
2024-10-14 21:57:35 +0700

    use max width wrapper

--- src/components/telegram-main.tsx
+++ src/components/telegram-main.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { useTelegramUser } from "@/telegram/use-telegram-user";
+import { MaxWidthWrapper } from "./ui/max-width-wrapper";
 
 export const TelegramMain = () => {
 	const { user } = useTelegramUser();
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24 w-full max-w-full bg-red-500 overflow-hidden">
-			{user?.id}
-			<p>Telegram App</p>
-			<p>Your Information: {user?.username}</p>
+		<main>
+			<MaxWidthWrapper>
+				{user?.id}
+				<p>Telegram App</p>
+				<p>Your Information: {user?.username}</p>
+			</MaxWidthWrapper>
 		</main>
 	);
 };
