474fffaf138c68806433c5170d95163a971bd6e5
Treacle <toandq@cyberk.io>
2024-10-14 23:29:18 +0700

    fix use telegram user

--- src/telegram/use-telegram-user.ts
+++ src/telegram/use-telegram-user.ts
@@ -1,8 +1,7 @@
-import { useState } from "react";
 import { getUserViaWindow } from "./get-user-via-window";
 
 export const useTelegramUser = () => {
-	const [user, setUser] = useState(getUserViaWindow());
+	const user = getUserViaWindow();
 
 	return { user };
 };
