61b7e85232c2209bd5cf7f8b91844bde89451c90
Treacle <toandq@cyberk.io>
2024-10-15 00:08:40 +0700

    remove toast

--- src/components/telegram-main.tsx
+++ src/components/telegram-main.tsx
@@ -2,27 +2,9 @@
 
 import { useTelegramUser } from "@/telegram/use-telegram-user";
 import { MaxWidthWrapper } from "./ui/max-width-wrapper";
-import { useToast } from "@/hooks/use-toast";
-import { useEffect, useState } from "react";
 
 export const TelegramMain = () => {
 	const { user } = useTelegramUser();
-	const { toast } = useToast();
-	const [mounted, setMounted] = useState(false);
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	useEffect(() => {
-		if (!user && mounted) {
-			toast({
-				title: "Error",
-				description: "User not found",
-				variant: "destructive",
-			});
-		}
-	}, [user, mounted]);
 
 	return (
 		<MaxWidthWrapper>
